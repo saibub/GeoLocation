c8c9185fd49368955806abea00ea72e193c36fe1
HImanshu <79564624+saibub@users.noreply.github.com>
2022-01-25 21:30:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//Using GeoLocation API from google
+
+
+function getlocation(){ 
+  if(navigator.geolocation){ 
+      navigator.geolocation.getCurrentPosition(showPos, showErr); 
+  }
+  else{
+      alert("Sorry! your Browser does not support Geolocation API")
+  }
+} 
+
+function showPos(position){ 
+  latt = position.coords.latitude; 
+  long = position.coords.longitude; 
+  var lattlong = new google.maps.LatLng(latt, long); 
+  var myOptions = { 
+      center: lattlong, 
+      zoom: 9, 
+      mapTypeControl: true, 
+      navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL} 
+  } 
+  var maps = new google.maps.Map(document.getElementById("demo"), myOptions); 
+  var markers = 
+  new google.maps.Marker({position:lattlong, map:maps, title:"You are here!"}); 
+
+  document.getElementById("output").innerHTML="(Latitude, Longitude) : "+lattlong;
+} 
+
+
+   function showErr(error) {
+    switch(error.code){
+    case error.PERMISSION_DENIED:
+   alert("User denied the request for Geolocation API.");
+    break;
+   case error.POSITION_UNAVAILABLE:
+   alert("USer location information is unavailable.");
+  break;
+  case error.TIMEOUT:
+  alert("The request to get user location timed out.");
+  break;
+ case error.UNKNOWN_ERROR:
+  alert("An unknown error occurred.");
+  break;
+ }
+}  
